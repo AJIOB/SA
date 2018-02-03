c2d289a7689fadfa6e430e1e671d365f5514612d
AJIOB <aleksandr9809@gmail.com>
2018-02-03 10:33:03 +0300

    Correlation coefficient calculating was added

--- lab1/lab.R
+++ lab1/lab.R
@@ -4,6 +4,11 @@ setwd("D:/labs/SA/lab1/")
 #read data
 data <- read.table("input.data", sep = "" , header = F, na.strings ="", stringsAsFactors= F)
 
-plot(data[, 1], data[, 6], xlab = "mpg", ylab = "acceleration", title("Link: \"http://archive.ics.uci.edu/ml/datasets/Auto+MPG\""))
+mpg <- data[, 1]
+acceleration <- data[, 6]
 
+plot(mpg, acceleration, xlab = "mpg", ylab = "acceleration", title("Link: \"http://archive.ics.uci.edu/ml/datasets/Auto+MPG\""))
+
+#correlation coefficient
+r <- cor(mpg, acceleration)
 
