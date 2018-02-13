39122124d9dbe20d6f03c95623fed4e22ac466a4
AJIOB <aleksandr9809@gmail.com>
2018-02-14 00:10:58 +0300

    lab2: Mathematical expectation, variation, Fisher and Student's criterias were added

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -1,10 +1,24 @@
 #set working directory
 setwd("D:/labs/SA/lab2/")
 
+confidential_interval = 0.95
+
 #read data
 data <- read.table("input.data", sep = "" , header = F, na.strings ="", stringsAsFactors= F)
 
 mpg <- data[, 1]
 acceleration <- data[, 6]
 
-#probabil
+# mathematical expectation
+M_mpg <- mean(mpg)
+M_acceleration <- mean(acceleration)
+
+# dispersion (variation)
+D_mpg <- var(mpg)
+D_acceleration <- var(acceleration)
+
+# Критерий Фишера
+Fisher <- var.test(mpg, acceleration, conf.level = confidential_interval)
+
+# Критерий Стьюдента
+t <- t.test(mpg, acceleration)
