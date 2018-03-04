65b8a376412bec6689055caf6036dbf52866110f
AJIOB <aleksandr9809@gmail.com>
2018-03-04 19:44:52 +0300

    Simple histogram was made

--- lab3/sa-lab3.R
+++ lab3/sa-lab3.R
@@ -4,19 +4,22 @@ setwd("D:/labs/SA/lab3/")
 # Install from CRAN
 library(imager)
 
-readJPEG_scale = 256
+jpegValueScale = 255
 
+#img1_path <- "Bad.jpg"
 img1_path <- "Tower_Bridge_640.jpg"
 img2_path <- "Zima_640.jpg"
 #img1_path <- "Tower_Bridge_4K.jpg"
 #img2_path <- "Zima_4K.jpg"
 
 customShow <- function(img, title){
-  plot(1:2, 1:2, type='n', main = title)
-  rasterImage(img, 1, 1, 2, 2)
+  plot(img, main = title)
 }
 
 img1_color <- load.image(img1_path)
 customShow(img1_color, "Image 1: color")
 img1_gray <- grayscale(img1_color)
 customShow(img1_gray, "Image 1: grayscale")
+
+img1_gray_scaled <- img1_gray * jpegValueScale ^ 2
+hist(img1_gray_scaled, main="Grayscale histogram of image 1", xlab = "", xlim = c(0, jpegValueScale))
