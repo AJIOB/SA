e0a2be2fa7c256851a1069c7c796a5ad6e319991
AJIOB <aleksandr9809@gmail.com>
2018-03-04 18:57:53 +0300

    Image grayscale convertion was made

--- lab3/sa-lab3.R
+++ lab3/sa-lab3.R
@@ -1,8 +1,8 @@
 #set working directory
 setwd("D:/labs/SA/lab3/")
 
-message("Code requires `jpeg` package")
-library(jpeg)
+# Install from CRAN
+library(imager)
 
 readJPEG_scale = 256
 
@@ -12,9 +12,11 @@ img2_path <- "Zima_640.jpg"
 #img2_path <- "Zima_4K.jpg"
 
 customShow <- function(img, title){
-  plot(1:2, type='n', main = title)
+  plot(1:2, 1:2, type='n', main = title)
   rasterImage(img, 1, 1, 2, 2)
 }
 
-img1_color <- readJPEG(img1_path)
+img1_color <- load.image(img1_path)
 customShow(img1_color, "Image 1: color")
+img1_gray <- grayscale(img1_color)
+customShow(img1_gray, "Image 1: grayscale")
