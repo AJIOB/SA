c1c3a6dbdcf69f68dc8838a769b478335b81d158
AJIOB <aleksandr9809@gmail.com>
2018-03-29 01:00:22 +0300

    file loading was moved to new function

--- lab4/sa-lab4.R
+++ lab4/sa-lab4.R
@@ -16,36 +16,43 @@ group1 <- "hardware"
 group2 <- "programming"
 group3 <- "economics"
 
-# load the text
-file_path <- paste(c(input_folder, group1, paste("1", file_extension, sep = "")), collapse = folder_delim)
-text <- readLines(file_path)
+thesausus_size = 10
 
-# Load the data as a corpus
-docs <- Corpus(VectorSource(text))
-
-# Convert the text to lower case
-docs <- tm_map(docs, content_transformer(tolower))
-# Remove numbers
-docs <- tm_map(docs, removeNumbers)
-# Remove english common stopwords
-docs <- tm_map(docs, removeWords, stopwords("english"))
-# Remove punctuations
-docs <- tm_map(docs, removePunctuation)
-# Eliminate extra white spaces
-docs <- tm_map(docs, stripWhitespace)
-# Text stemming (almost basic word form)
-docs <- tm_map(docs, stemDocument)
-
-# calculate word frequencies
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
-head(d, 10)
-
+# read first $thesausus_size words from file (with their freq-s)
+load_text <- function(group_name, file_name)
+{
+  # load the text
+  file_path <- paste(c(input_folder, group_name, paste(file_name, file_extension, sep = "")), collapse = folder_delim)
+  text <- readLines(file_path)
+  
+  # Load the data as a corpus
+  docs <- Corpus(VectorSource(text))
+  
+  # Convert the text to lower case
+  docs <- tm_map(docs, content_transformer(tolower))
+  # Remove numbers
+  docs <- tm_map(docs, removeNumbers)
+  # Remove english common stopwords
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+  # Remove punctuations
+  docs <- tm_map(docs, removePunctuation)
+  # Eliminate extra white spaces
+  docs <- tm_map(docs, stripWhitespace)
+  # Text stemming (almost basic word form)
+  docs <- tm_map(docs, stemDocument)
+  
+  # calculate word frequencies
+  dtm <- TermDocumentMatrix(docs)
+  m <- as.matrix(dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v)
+  head(d, thesausus_size)
+  
 # word cloud building
-set.seed(1234)
-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
+#set.seed(1234)
+#wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+#          max.words=200, random.order=FALSE, rot.per=0.35, 
+#          colors=brewer.pal(8, "Dark2"))
+}
 
+d_temp <- load_text(group1, "1")
